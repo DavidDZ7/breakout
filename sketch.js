e0a3c23a1380dd7c22a04defdcd508e7553614a4
David Norman Diaz Estrada <daviddiaz997@gmail.com>
2023-08-06 14:12:06 -0600

    Added game over and win notifications, changed colors

--- sketch.js
+++ sketch.js
@@ -8,7 +8,10 @@ August 2023
 backgroundColor='#444444'
 paddleColor='#999999';
 ballColor="#CCCCCC";
-targetColors=["#CC0000","#CCCC00","#00CC00","#0000CC"];
+targetColors=["#f95929","#f9d300","#00cd00","#26c0ff"]//["#CC0000","#CCCC00","#00CC00","#0000CC"];
+
+const rows=targetColors.length;
+const cols=10;
 
 paddle1=null
 ball1=null
@@ -18,65 +21,99 @@ score=0
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  targets = createTargets()
+  [targets,maxWinPoints] = createTargets(rows,cols);
+  console.log("maxWinpoints: ",maxWinPoints);
   paddle1 = new paddle();
   ball1 = new ball();
 }
 
 function draw() {
-  background(backgroundColor);
-  showGameInfo();
-  for (let i =0; i <targets.length; i++) {
-    const currentTarget = targets[i];
-
-    let collision=currentTarget.detectCollision(ball1); 
-    if (collision){
-      //increase score:
-      score+=1
-      //bounce ball:
-      ball1.Yspeed=abs(ball1.Yspeed);
-      //decrease target life:
-      currentTarget.life-=1;
-      if (currentTarget.life<0){//hide target
-        currentTarget.visible=false;
-        currentTarget.checkCollisions=false;}
-      break;
+  let gameIsOver=ball1.check_if_ball_passed_bottom();
+  if (gameIsOver){
+    ShowGameOver();//display text
+    setTimeout(restartGame, 1500);//restart after 1.5 seconds
+    noLoop();//stop the draw function
+  }
+  else if(score==maxWinPoints){
+    showGameInfo();
+    ShowWin();
+    noLoop();//stop the draw function
+  }
+  else{
+    background(backgroundColor);
+    showGameInfo();
+    //Check collisions with targets:
+    for (let i =0; i <targets.length; i++) {
+      const currentTarget = targets[i];
+  
+      let collision=currentTarget.detectCollision(ball1); 
+      if (collision){
+        //increase score:
+        score+=1;
+        //bounce ball:
+        ball1.Yspeed=abs(ball1.Yspeed);
+        //decrease target life:
+        currentTarget.life-=1;
+        if (currentTarget.life<1){//hide target
+          currentTarget.visible=false;
+          currentTarget.checkCollisions=false;}
+        break;
+      }
+  
     }
-
+  
+    //Show visible targets:
+    for (const currentTarget of targets) {
+      if (currentTarget.visible){currentTarget.show();}
+    }
+    //Update ball:
+    ball1.update(paddle1);
+    ball1.show();
+    //Update paddle:    
+    paddle1.update();
+    paddle1.show();
   }
-
-
-  for (const currentTarget of targets) {
-    if (currentTarget.visible){currentTarget.show();}
-  }
-  paddle1.show();
-  ball1.update(paddle1);
-  ball1.check_if_ball_passed_bottom();
-  //score=ball1.checkIfScored();
-  ball1.show();
-
-  checkKeys();
 }
 
-function createTargets(){
-  let rows=targetColors.length;
-  let cols=10;
+function createTargets(rows,cols){
+  let maxWinPoints=0
   let targetWidth=windowWidth/cols;
   let targetHeight=(windowHeight/4)/rows;
   targets=[]
-  targetLife=targetColors.length-1;
+  targetLife=targetColors.length;
 
   for (row=0;row<rows;row++){
     let checkCollisions=true;
     for (col=0;col<cols;col++){
-      //if (row==rows-1){checkCollisions=true}//at game start ensure to check collisions in all targets in last row
       targets.push(new target(row,col,targetWidth,targetHeight,checkCollisions,targetLife))
+      maxWinPoints=maxWinPoints+targetLife
     }
     targetLife-=1;
   }
-  return targets
+  return [targets,maxWinPoints]
 }
 
+function ShowGameOver(){
+  let gameOverText="GAME OVER!";
+  strokeWeight(0);
+  textSize(40);
+  let textWidth_ = textWidth(gameOverText);
+  let textHeight_ = textAscent(gameOverText) + textDescent(gameOverText);
+  //display text at the center of window
+  fill(20);
+  text(gameOverText,x=windowWidth/2-textWidth_/2,y=windowHeight/2-textHeight_/2);
+}
+
+function ShowWin(){
+  let winText="YOU WON!";
+  strokeWeight(0);
+  textSize(40);
+  let textWidth_ = textWidth(winText);
+  let textHeight_ = textAscent(winText) + textDescent(winText);
+  //display text at the center of window
+  fill(20);
+  text(winText,x=windowWidth/2-textWidth_/2,y=windowHeight/2-textHeight_/2);
+}
 
 function showGameInfo(){
   //----------------------------------------------------------------
@@ -104,14 +141,6 @@ function showGameInfo(){
 }
 
 
-function checkKeys() {
-  if (keyIsDown(RIGHT_ARROW)) {
-    paddle1.update(direction=1); // Move paddle to the right
-  } else if (keyIsDown(LEFT_ARROW)) {
-    paddle1.update(direction=-1); // Move paddle to the left
-  }
-}
-
 
 function windowResized() {//restart game if window is resized
   resizeCanvas(windowWidth, windowHeight);
@@ -125,7 +154,8 @@ function restartGame(){
   score1=null
   targets=[]
   score=0
-  setup()
+  setup()//initialize game configuration
+  loop()//resume the draw function
 }
 
 class paddle{
@@ -146,7 +176,15 @@ class paddle{
     rect(this.x, this.y, this.width, this.height,this.radius);
   }
 
-  update(direction){
+  update() {
+    if (keyIsDown(RIGHT_ARROW)) {    
+      this.move(1); // Move paddle to the right
+    } else if (keyIsDown(LEFT_ARROW)) {
+      this.move(-1); // Move paddle to the left
+    }
+  }
+
+  move(direction){
     this.x=this.x+direction*this.speed;
     //ensure the paddle does not go outside the window:
     this.x=max(min(this.x,windowWidth-this.width/2),this.width/2);
@@ -159,22 +197,21 @@ class ball{
   constructor(){
     this.diameter=40;
     this.color=ballColor;
-    this.speed=6//max(5,0.008*windowWidth);//speed should increase for larger window width
-    this.direction=1;
-    this.reset(this.direction);
+    this.speed=6;
+    this.reset();
   }
 
-  reset(direction){
+  reset(){
     this.x=windowWidth/2;
     this.y=windowHeight/3;
-    let angle=Math.PI/2//random(-Math.PI/8,Math.PI/8)//between -22.5 and 22.5 degrees
+    let angle=Math.PI/2;//90 degrees
     this.Xspeed=this.speed*cos(angle);
-    this.Yspeed=this.speed*sin(angle)*direction;//direction is -1 or 1
+    this.Yspeed=this.speed*sin(angle);
   }
 
   show(){
     fill(this.color);
-    strokeWeight(4); 
+    strokeWeight(0); 
     circle(this.x, this.y, this.diameter);
   }
 
@@ -193,7 +230,6 @@ class ball{
     else if(this.y-this.diameter/2<=0){
       this.Yspeed=abs(this.Yspeed);}      
     //ensure ball bounces when hitting paddle:
-    
     if(this.x>=paddle1.x-paddle1.width/2 && this.x<=paddle1.x+paddle1.width/2 && this.y+this.diameter/2>=windowHeight-3*paddle1.height){
       let normalizedCenterDiff=(paddle1.x-this.x)/paddle1.width//[-1,1] range
       let angle=(Math.PI/2)*normalizedCenterDiff //range [-90,90] degrees
@@ -205,10 +241,9 @@ class ball{
 
   check_if_ball_passed_bottom(){
     if (this.y+this.diameter/2>=windowHeight){
-      restartGame()
-      //let direction = 1
-      //this.reset(direction=1)
+      return true    
     }
+    return false
   }
 
 }
@@ -227,7 +262,7 @@ class target{
   }
 
   show() {
-    fill(targetColors[this.life]);
+    fill(targetColors[this.life-1]);
     strokeWeight(3);
     stroke(backgroundColor)
     rectMode(CENTER);
